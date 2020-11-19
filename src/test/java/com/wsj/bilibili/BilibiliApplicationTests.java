a6ec6a91eb8d4d2aa9bdbc660c16ae2f417985a9
王施鉴 <wsj249565@gongdao.com>
2020-11-20 00:04:25 +0800

    初始化

---
+++ src/test/java/com/wsj/bilibili/BilibiliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsj.bilibili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilibiliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
